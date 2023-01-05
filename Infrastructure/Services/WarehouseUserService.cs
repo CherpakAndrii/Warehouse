1d572e0947cd1fee08d68909274dfc05179a5375
CherpakAndriiGmBst <a.cherpak-2026@kpi.ua>
2023-01-05 04:43:03 +0200

    Виніс спільні для сервісів функції в окремий клас

---
+++ Infrastructure/Services/WarehouseUserService.cs
@@ -0,0 +1,39 @@
+﻿using Infrastructure.Interfaces;
+using Models.Api.Common.Response;
+using Models.Api.Admin.Request;
+using Models.Api.Admin.Response.Success;
+using Models.Api.Common.Request;
+using Models.DBModels;
+
+namespace Infrastructure.Services
+{
+    public abstract class WarehouseUserService : IWarehouseUserService
+    {
+        protected readonly IProductsRepository _productsRepository;
+        protected readonly ICustomersRepository _customersRepository;
+        protected readonly IOrdersRepository _ordersRepository;
+
+        public WarehouseUserService(IProductsRepository productsRepository, ICustomersRepository customersRepository, IOrdersRepository ordersRepository)
+        {
+            _productsRepository = productsRepository;
+            _customersRepository = customersRepository;
+            _ordersRepository = ordersRepository;
+        }
+
+        public ErrorResponseModel TryFindProduct(ActionWithExistingProductRequestModel productRequest)
+        {
+            var product = _productsRepository.GetProduct(productRequest.ProductId);
+            if (product is null) return new() { ErrorMessage = "product not found" };
+
+            return null;
+        }
+
+        public ErrorResponseModel TryFindOrder(ActionWithExistingOrderRequestModel orderRequest)
+        {
+            var order = _ordersRepository.GetOrder(orderRequest.OrderId);
+            if (order is null) return new() { ErrorMessage = "order not found" };
+
+            return null;
+        }
+    }
+}
